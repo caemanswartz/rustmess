dc8973fbb86004683b7c7e4cb1db73159e33dd6d
Caeman Swartz <caemanswartz@gmail.com>
2020-11-05 14:24:03 -0800

    added functions for file loading of textures

--- src/main.rs
+++ src/main.rs
@@ -2,24 +2,62 @@
 extern crate glium;
 
 use std::io::Cursor;
+use glium::{glutin, Surface};
+use std::{
+    env,
+    fs::File,
+    io::prelude::*,
+    path::Path
+};
+
+#[derive(Copy, Clone)]
+struct Vertex {
+    position: [f32; 3],
+    normal: [f32; 3],
+    tex_coords: [f32; 2],
+}
+fn load_bytes(file_path: &str) -> Vec<u8> {
+    let path = Path::new(file_path);
+    let mut file = match File::open(&path) {
+        Err(why) => panic!("Couldn't open {}: {}", path.display(), why),
+        Ok(file) => file,
+    };
+    let mut buffer = Vec::new();
+    match file.read_to_end(&mut buffer) {
+        Err(why) => panic!("Couldn't read {}: {}", path.display(), why),
+        _ => buffer,
+    }
+}
+fn load_diffuse_map(display: &glium::Display, format: image::ImageFormat, file_path: &str) -> glium::texture::SrgbTexture2d {
+    let buffer = load_bytes(file_path);
+    let image = image::load(Cursor::new(buffer),
+                            format).unwrap().to_rgba();
+    let image_dimensions = image.dimensions();
+    let raw_image = glium::texture::RawImage2d::from_raw_rgba_reversed(&image.into_raw(), image_dimensions);
+    glium::texture::SrgbTexture2d::new(display, raw_image).unwrap()
+}
+fn load_normal_map(display: &glium::Display, format: image::ImageFormat, file_path: &str) -> glium::texture::Texture2d {
+    let buffer = load_bytes(file_path);
+    let image = image::load(Cursor::new(buffer),
+                            format).unwrap().to_rgba();
+    let image_dimensions = image.dimensions();
+    let raw_image = glium::texture::RawImage2d::from_raw_rgba_reversed(&image.into_raw(), image_dimensions);
+    glium::texture::Texture2d::new(display, raw_image).unwrap()
+}
+
+implement_vertex!(Vertex, position, normal, tex_coords);
 
 fn main() {
     #[allow(unused_imports)]
-    use glium::{glutin, Surface};
+
+    println!("{}", env::current_dir().unwrap().display());
 
     let event_loop = glutin::event_loop::EventLoop::new();
     let wb = glutin::window::WindowBuilder::new();
     let cb = glutin::ContextBuilder::new().with_depth_buffer(24);
     let display = glium::Display::new(wb, cb, &event_loop).unwrap();
 
-    #[derive(Copy, Clone)]
-    struct Vertex {
-        position: [f32; 3],
-        normal: [f32; 3],
-        tex_coords: [f32; 2],
-    }
-
-    implement_vertex!(Vertex, position, normal, tex_coords);
+// building model data, pull out
 
     let shape = glium::vertex::VertexBuffer::new(&display, &[
             Vertex { position: [-1.0,  1.0, 0.0], normal: [0.0, 0.0, -1.0], tex_coords: [0.0, 1.0] },
@@ -28,19 +66,18 @@ fn main() {
             Vertex { position: [ 1.0, -1.0, 0.0], normal: [0.0, 0.0, -1.0], tex_coords: [1.0, 0.0] },
         ]).unwrap();
 
-
+// load images, pull out
+/*
     let image = image::load(Cursor::new(&include_bytes!("./tuto-14-diffuse.jpg")[..]),
                             image::ImageFormat::Jpeg).unwrap().to_rgba();
     let image_dimensions = image.dimensions();
     let image = glium::texture::RawImage2d::from_raw_rgba_reversed(&image.into_raw(), image_dimensions);
     let diffuse_texture = glium::texture::SrgbTexture2d::new(&display, image).unwrap();
+ */
+    let diffuse_map = load_diffuse_map(&display, image::ImageFormat::Jpeg, "src/tuto-14-diffuse.jpg");
+    let normal_map = load_normal_map(&display, image::ImageFormat::Png, "src/tuto-14-normal.png");
 
-    let image = image::load(Cursor::new(&include_bytes!("./tuto-14-normal.png")[..]),
-                            image::ImageFormat::Png).unwrap().to_rgba();
-    let image_dimensions = image.dimensions();
-    let image = glium::texture::RawImage2d::from_raw_rgba_reversed(&image.into_raw(), image_dimensions);
-    let normal_map = glium::texture::Texture2d::new(&display, image).unwrap();
-
+// building program data, pull out
 
     let vertex_shader_src = String::from_utf8_lossy(&include_bytes!("./vertex_shader.glsl")[..]);
 
@@ -48,11 +85,14 @@ fn main() {
 
     let program = glium::Program::from_source(&display, &vertex_shader_src, &fragment_shader_src,
                                               None).unwrap();
+// event loop, leave in
 
     event_loop.run(move |event, _, control_flow| {
         let next_frame_time = std::time::Instant::now() +
             std::time::Duration::from_nanos(16_666_667);
         *control_flow = glutin::event_loop::ControlFlow::WaitUntil(next_frame_time);
+
+// event handling, leave in
 
         match event {
             glutin::event::Event::WindowEvent { event, .. } => match event {
@@ -70,8 +110,12 @@ fn main() {
             _ => return,
         }
 
+// prepare to draw to screen, leave in
+
         let mut target = display.draw();
         target.clear_color_and_depth((0.0, 0.0, 1.0, 1.0), 1.0);
+
+// preparing to draw, pull out
 
         let model = [
             [1.0, 0.0, 0.0, 0.0],
@@ -111,14 +155,17 @@ fn main() {
             .. Default::default()
         };
 
+// draw to screen, leave in
+
         target.draw(&shape, glium::index::NoIndices(glium::index::PrimitiveType::TriangleStrip), &program,
                     &uniform! { model: model, view: view, perspective: perspective,
-                                u_light: light, diffuse_tex: &diffuse_texture, normal_tex: &normal_map },
+                                u_light: light, diffuse_tex: &diffuse_map, normal_tex: &normal_map },
                     &params).unwrap();
         target.finish().unwrap();
     });
 }
 
+// helper function, to be with model and drawing code
 
 fn view_matrix(position: &[f32; 3], direction: &[f32; 3], up: &[f32; 3]) -> [[f32; 4]; 4] {
     let f = {
